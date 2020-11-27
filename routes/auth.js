c1c5a882067d862b80adf1063a3bf5d36a2b90bc
Noman Munir <mrnomanmunir@gmail.com>
2020-11-27 15:16:23 +0400

    validated User with @hapi/joi and checked if user already exist or not

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const { registerValidation } = require('../validation');
+
 
 router.post('/register', async (req, res) => {
+    // Register Validation
+    const { error } = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    // Check if User Already Exisit or not
+    const emailExist = await User.findOne({ email: req.body.email })
+    if ((emailExist)) return res.status(400).send("Email Already Exist");
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
