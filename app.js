27bc56490b23426db003987b8433410c4eb2d15b
Noman Munir <mrnomanmunir@gmail.com>
2020-11-27 11:25:45 +0400

    created user model and install mongoose

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const env = require('dotenv');
+const mongoose = require('mongoose');
+
 const app = express();
+const authRoute = require('./routes/auth');
 
 env.config();
+
+// Connect to DB
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true , useUnifiedTopology: true}, () => console.log('Db Connected'));
 
 // Middleware
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+// Route Middlewares
+app.use('/api/user', authRoute);
 
 // Server 
 app.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT}`));
---
+++ model/User.js
@@ -0,0 +1,29 @@
+const { mongo } = require("mongoose");
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+        min: 6,
+        max: 255
+    },
+    email: {
+        type: String,
+        require: true,
+        min: 6,
+        max: 255
+    },
+    password: {
+        type: String,
+        require: true,
+        min: 8,
+        max: 100,
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+module.exports = mongoose.model('User', userSchema);
